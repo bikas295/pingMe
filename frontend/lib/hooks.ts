1a2a4106829217f6fa58ebc169b1c1f71f520d88
bikas295 <112216014@ece.iiitp.ac.in>
2025-05-27 11:45:25 +0530

    added public api url var

--- frontend/lib/hooks.ts
+++ frontend/lib/hooks.ts
@@ -6,11 +6,14 @@ type Request = {
   timestamp: string;
 };
 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
 export const useRequests = () =>
   useQuery<Request[]>({
     queryKey: ['requests'],
     queryFn: async () => {
-      const res = await fetch('http://localhost:3001/api/requests');
+      const res = await fetch(`${apiUrl}/api/requests`);
+      //const res = await fetch('http://localhost:3001/api/requests');
       if (!res.ok) throw new Error('Failed to fetch requests');
       return res.json();
     },
